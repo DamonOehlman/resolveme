fc0a6a215a7405efe56ebe60ce727bf0702c4c27
Damon Oehlman <damon.oehlman@gmail.com>
2014-06-09 15:24:20 +1000

    Tabs -> spaces

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var async = require('async'),
-	findme = require('findme'),
-	events = require('events'),
-	debug = require('debug')('resolveme'),
-	localResolver = require('./resolvers/local'),
-	getFileSeparator = require('./manifest').getFileSeparator,
-	util = require('util'),
-	_ = require('underscore'),
-	reCommaDelim = /\,\s*/;
+    findme = require('findme'),
+    events = require('events'),
+    debug = require('debug')('resolveme'),
+    localResolver = require('./resolvers/local'),
+    getFileSeparator = require('./manifest').getFileSeparator,
+    util = require('util'),
+    _ = require('underscore'),
+    reCommaDelim = /\,\s*/;
 
 /* private helpers */
 
@@ -16,165 +16,165 @@ function parseTargets(targets) {
 /* resolveme Bundle definition */
 
 function Bundle(targets) {
-	// call the inherits event emitter constructor
-	events.EventEmitter.call(this);
+  // call the inherits event emitter constructor
+  events.EventEmitter.call(this);
 
-	// initialise the targets
-	this.targets = [];
+  // initialise the targets
+  this.targets = [];
 
-	// ensure the targets array is valid
-	targets = targets || [];
+  // ensure the targets array is valid
+  targets = targets || [];
 
-	// add each of the targets
-	for (var ii = 0; ii < targets.length; ii++) {
-		this.add(targets[ii]);
-	}
+  // add each of the targets
+  for (var ii = 0; ii < targets.length; ii++) {
+    this.add(targets[ii]);
+  }
 }
 
 util.inherits(Bundle, events.EventEmitter);
 
 Bundle.prototype._locate = function(target, opts, callback) {
-	async.detect(
-		opts.resolvers || [localResolver],
+  async.detect(
+    opts.resolvers || [localResolver],
 
-		function(resolver, itemCallback) {
-			resolver.exists(target, opts, itemCallback);
-		},
+    function(resolver, itemCallback) {
+      resolver.exists(target, opts, itemCallback);
+    },
 
-		callback
-	);
+    callback
+  );
 };
 
 Bundle.prototype.add = function(target, atStart) {
-	// if the target is empty, return this
-	if (! target) return this;
+  // if the target is empty, return this
+  if (! target) return this;
 
-	if (typeof target == 'string' || (target instanceof String)) {
-		target = findme.define(target.trim());
-	}
+  if (typeof target == 'string' || (target instanceof String)) {
+    target = findme.define(target.trim());
+  }
 
-	// add the target to the targets list
-	if (target && target.name) {
-		var existing = this.targets.filter(function(item) {
-			return item.name.toLowerCase() === target.name.toLowerCase();
-		})[0];
+  // add the target to the targets list
+  if (target && target.name) {
+    var existing = this.targets.filter(function(item) {
+      return item.name.toLowerCase() === target.name.toLowerCase();
+    })[0];
 
-		// if we have an existing item with the same name, return
-		// TODO: check versions see if we should override, etc
-		if (existing) return this;
+    // if we have an existing item with the same name, return
+    // TODO: check versions see if we should override, etc
+    if (existing) return this;
 
-		// add the target
-		Array.prototype[atStart ? 'unshift' : 'push'].call(this.targets, target);
-	}
+    // add the target
+    Array.prototype[atStart ? 'unshift' : 'push'].call(this.targets, target);
+  }
 
-	return this;
+  return this;
 };
 
 Bundle.prototype.getContent = function(fileType) {
-	var items = this.targets.map(function(target) {
-		return target.manifest.getContent(fileType || 'js');
-	});
+  var items = this.targets.map(function(target) {
+    return target.manifest.getContent(fileType || 'js');
+  });
 
-	return resolveme.join(items, fileType || 'js');
+  return resolveme.join(items, fileType || 'js');
 };
 
 Bundle.prototype.resolve = function(opts, callback) {
-	// find the targets that still required resolution	
-	var bundle = this,
-		targets = this.targets.filter(function(target) {
-			return target && typeof target.manifest == 'undefined';
-		});
+  // find the targets that still required resolution
+  var bundle = this,
+      targets = this.targets.filter(function(target) {
+        return target && typeof target.manifest == 'undefined';
+      });
 
-	// if we have no targets, then trigger the callback and exit
-	if (targets.length === 0) {
-		if (callback) callback(null, this);
+  // if we have no targets, then trigger the callback and exit
+  if (targets.length === 0) {
+    if (callback) callback(null, this);
 
-		return this.emit('complete');
-	}
+    return this.emit('complete');
+  }
 
-	// get the sources for each of the specified targets
-	async.forEach(
-		targets,
+  // get the sources for each of the specified targets
+  async.forEach(
+    targets,
 
-		function(target, itemCallback) {
-			bundle._locate(target, opts, function(resolver) {
-				// if we didn't get a resolver report that too
-				if (! resolver) return callback(new Error('unable to resolve: ' + target));
+    function(target, itemCallback) {
+      bundle._locate(target, opts, function(resolver) {
+        // if we didn't get a resolver report that too
+        if (! resolver) return callback(new Error('unable to resolve: ' + target));
 
-				resolver.retrieve(target, opts, function(err, manifest, deps) {
-					// update the manifest
-					target.manifest = manifest || {};
+        resolver.retrieve(target, opts, function(err, manifest, deps) {
+          // update the manifest
+          target.manifest = manifest || {};
 
-					// add the dependencies to the target
-					target._deps = deps || [];
+          // add the dependencies to the target
+          target._deps = deps || [];
 
-					// iterate through the dependencies and create new targets
-					target._deps.forEach(function(item) {
-						bundle.add(item, true);
-					});
+          // iterate through the dependencies and create new targets
+          target._deps.forEach(function(item) {
+            bundle.add(item, true);
+          });
 
-					// trigger the callback
-					itemCallback(err);
-				});
-			});
-		},
+          // trigger the callback
+          itemCallback(err);
+        });
+      });
+    },
 
-		function(err) {
-			if (err) return callback(err);
+    function(err) {
+      if (err) return callback(err);
 
-			bundle.resolve(opts, callback);
-		}
-	);
+      bundle.resolve(opts, callback);
+    }
+  );
 };
 
 Object.defineProperty(Bundle.prototype, 'fileTypes', {
-	get: function() {
-		var fileTypes = [];
+  get: function() {
+    var fileTypes = [];
 
-		this.targets.forEach(function(target) {
-			if (target.manifest) {
-				fileTypes = fileTypes.concat(target.manifest.fileTypes);
-			}
-		});
+    this.targets.forEach(function(target) {
+      if (target.manifest) {
+        fileTypes = fileTypes.concat(target.manifest.fileTypes);
+      }
+    });
 
-		return _.uniq(fileTypes.sort(), true);
-	}
+    return _.uniq(fileTypes.sort(), true);
+  }
 });
 
 /* main */
 
 function resolveme(targets, opts, callback) {
-	var bundle;
+  var bundle;
 
-	// if we have been provied a string of targets, then split on commas
-	if (typeof targets == 'string' || (targets instanceof String)) {
-		targets = targets.split(reCommaDelim);
-	}
+  // if we have been provied a string of targets, then split on commas
+  if (typeof targets == 'string' || (targets instanceof String)) {
+    targets = targets.split(reCommaDelim);
+  }
 
-	// check for the 2 arg variant of the function
-	if (typeof opts == 'function') {
-		callback = opts;
-		opts = {};
-	}
+  // check for the 2 arg variant of the function
+  if (typeof opts == 'function') {
+    callback = opts;
+    opts = {};
+  }
 
-	// create the new bundle
-	bundle = new Bundle(targets);
+  // create the new bundle
+  bundle = new Bundle(targets);
 
-	// resolve the bundle (if we have targets)
-	if (bundle.targets.length > 0) {
-		bundle.resolve(opts, callback);
-	}
-	else if (callback) {
-		callback(null, bundle);
-	}
+  // resolve the bundle (if we have targets)
+  if (bundle.targets.length > 0) {
+    bundle.resolve(opts, callback);
+  }
+  else if (callback) {
+    callback(null, bundle);
+  }
 
-	// return the scope which passes on useful events
-	return bundle;
+  // return the scope which passes on useful events
+  return bundle;
 }
 
 // add the resolveme join helper
 resolveme.join = function(parts, fileType) {
-	return parts.join(getFileSeparator(fileType));
+  return parts.join(getFileSeparator(fileType));
 };
 
 module.exports = resolveme;
